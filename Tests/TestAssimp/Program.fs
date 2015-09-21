b2e852fa6f6776dd7d9e103049e2584be4db5bc9
Wael El Oraiby <wael.eloraiby@gmail.com>
2015-09-20 21:19:45 -0400

    start converting assimp scene to F# RTK scene graphs

--- Tests/TestAssimp/Program.fs
+++ Tests/TestAssimp/Program.fs
@@ -23,6 +23,8 @@ type Vertex =
         val     Bones       : ivec4
     end
 
+    new(p, n, tn, btn, tc, w, b)    = { Position = p; Normal = n; Tangent = tn; BiTangent = btn; TexCoords = tc; Weights = w; Bones = b }
+
 type RenderMesh = Mesh32<Vertex>
 
 [<EntryPoint>]
@@ -39,29 +41,54 @@ let main argv =
     printfn "has meshes     : %b" scene.HasMeshes
     printfn "has textures   : %b" scene.HasTextures
 
+    let rec foldNodes (n: Assimp.Node) (l: int) =
+        let str = System.String (Array.init (4 * l) (fun c -> ' '))
+
+        for i in n.Children do
+            printfn "%s- node.name           : %s" str n.Name
+
+        for i in n.Children do
+            foldNodes i (l + 1)
+
+    foldNodes scene.RootNode 0
+
     for mtl in scene.Materials do
         printfn "- material.name       : %s" mtl.Name
         printfn "- material.diffuse    : %s" mtl.TextureDiffuse.FilePath
 
-    for mesh in scene.Meshes do
-        printfn "- mesh.name           : %s" mesh.Name
-        printfn "- mesh.bone count     : %d" mesh.BoneCount
-        printfn "- mesh.face count     : %d" mesh.FaceCount
-        printfn "- mesh.vertex count   : %d" mesh.VertexCount
+    let meshes =
+        scene.Meshes
+        |> Seq.map
+            (fun mesh ->       
+                printfn "- mesh.name           : %s" mesh.Name
+                printfn "- mesh.bone count     : %d" mesh.BoneCount
+                printfn "- mesh.face count     : %d" mesh.FaceCount
+                printfn "- mesh.vertex count   : %d" mesh.VertexCount
 
-        let positions   = mesh.Vertices |> Seq.map (fun v -> vec3(v.X, v.Y, v.Z)) |> Seq.toArray
-        let normals     = mesh.Normals  |> Seq.map (fun n -> vec3(n.X, n.Y, n.Z)) |> Seq.toArray
+                let positions   = mesh.Vertices                      |> Seq.map (fun v  -> vec3(v.X, v.Y, v.Z))    |> Seq.toArray
+                let normals     = mesh.Normals                       |> Seq.map (fun n  -> vec3(n.X, n.Y, n.Z))    |> Seq.toArray
+                let tangents    = mesh.Tangents                      |> Seq.map (fun t  -> vec3(t.X, t.Y, t.Z))    |> Seq.toArray
+                let biTangents  = mesh.BiTangents                    |> Seq.map (fun bt -> vec3(bt.X, bt.Y, bt.Z)) |> Seq.toArray
+                let biNormals   = mesh.BiTangents                    |> Seq.map (fun bn -> vec3(bn.X, bn.Y, bn.Z)) |> Seq.toArray
+                let tcoords     = mesh.TextureCoordinateChannels.[0] |> Seq.map (fun tc -> vec2(tc.X, tc.Y))       |> Seq.toArray
 
-//        let m = { RenderMesh.Vertices =
-//                    mesh.Vertices
-//                    |> Seq.map
-//                        (fun v ->
-//                            v.X)
-//                    |> Seq.toArray
-//                  Triangles = [||] }
+                let m = { RenderMesh.Vertices =
+                            mesh.Vertices
+                            |> Seq.mapi
+                                (fun i v ->
+                                    Vertex( positions.[i]
+                                          , normals.[i]
+                                          , tangents.[i]
+                                          , biTangents.[i]
+                                          , tcoords.[i]
+                                          , vec4()
+                                          , ivec4()))
+                            |> Seq.toArray
+                          Triangles = [||] }
 
-        for b in mesh.Bones do
-            printfn "-- bone.Name           : %s" b.Name
-            printfn "-- bone.weight count   : %d" b.VertexWeightCount
+                for b in mesh.Bones do
+                    printfn "-- bone.Name           : %s" b.Name
+                    printfn "-- bone.weight count   : %d" b.VertexWeightCount
+                m)
 
     0 // return an integer exit code
