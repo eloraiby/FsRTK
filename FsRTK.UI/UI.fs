5b51fd687e40df5a5c8474c378a50baf0d493e1e
eloraiby <wael.eloraiby@gmail.com>
2015-09-24 21:42:18 -0400

    add missing license to UI

--- FsRTK.UI/UI.fs
+++ FsRTK.UI/UI.fs
@@ -1,13 +1,31 @@
-﻿namespace FsRTK.UI
+﻿(*
+** Math3D for F#
+** Copyright (C) 2015  Wael El Oraiby
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Affero General Public License as
+** published by the Free Software Foundation, either version 3 of the
+** License, or (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Affero General Public License for more details.
+** 
+** You should have received a copy of the GNU Affero General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+namespace FsRTK.UI
 
 open FsRTK
 open FsRTK.Math3D.Vector
 open FsRTK.Math3D.Matrix
 open FsRTK.Math3D.Geometry
 
-type ControlAcceptInput =
-    | Enable
-    | Disable
+type InputReception =
+    | Accept
+    | Discard
 
 type Slider = {
     Min : float
@@ -16,14 +34,14 @@ type Slider = {
 }
 
 type Layout = {
-    Controls : (ControlAcceptInput * Control)[]
+    Controls : (InputReception * Control)[]
     Apply    : size2 * Control [] -> rect[]
 }
 
 and Control =
     | Label     of string
     | Checkbox  of string * bool
-    | Radiobox  of string * (ControlAcceptInput * string * bool) []
+    | Radiobox  of string * (InputReception * string * bool) []
     | Button    of string * bool
     | HSlider   of Slider
     | Collapse  of string * Control[]
@@ -54,7 +72,7 @@ type Frame = {
 
 type Theme = {
     Name        : string
-    Present     : (rect * ControlAcceptInput * Control)[] -> unit
+    Present     : (rect * InputReception * Control)[] -> unit
 }
 
 type FrameManagerState = {
@@ -104,9 +122,9 @@ type private RenderState =
 with
     static member mapControlState e (isHot, isActive) =
         match e with
-        | ControlAcceptInput.Enable ->
+        | InputReception.Accept ->
             match isHot, isActive with
             | _, true -> Active
             | true, false -> Hot
             | false, false -> Normal
-        | ControlAcceptInput.Disable -> Disabled
+        | InputReception.Discard -> Disabled
