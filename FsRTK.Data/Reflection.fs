b7dd93e75e86193f2da002525155dceeba61ddfa
eloraiby <wael.eloraiby@gmail.com>
2015-09-30 08:02:05 -0400

    record json output

--- FsRTK.Data/Reflection.fs
+++ FsRTK.Data/Reflection.fs
@@ -1,4 +1,22 @@
-﻿module FsRTK.Data.Reflection
+﻿(*
+** F# Rendering ToolKit
+** Copyright (C) 2015  Wael El Oraiby
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Affero General Public License as
+** published by the Free Software Foundation, either version 3 of the
+** License, or (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Affero General Public License for more details.
+** 
+** You should have received a copy of the GNU Affero General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+module FsRTK.Data.Json
 
 open System
 open System.Reflection
@@ -7,37 +25,41 @@ open Microsoft.FSharp.Reflection
 open FsRTK.Data
 open FsRTK.Data.Runtime
 
-//let rec valueSerializer (t: Type) (v: obj) =
-//    match t with
-//    | t when t = typeof<int>        -> JsonValue.Number  (System.Convert.ToDecimal v)
-//    | t when t = typeof<single>     -> JsonValue.Float   (System.Convert.ToDouble  v)
-//    | t when t = typeof<float>      -> JsonValue.Float   (unbox<float>  v)
-//    | t when t = typeof<bool>       -> JsonValue.Boolean (unbox<bool>   v)
-//    | t when t = typeof<string>     -> JsonValue.String  (unbox<string> v)
-//    | t when t.IsArray              ->
-//        JsonValue.Array
-//        <| 
-                                        
-let rec createRecordInstance t =
+let serializeInt      (v: obj) = JsonValue.Number  (System.Convert.ToDecimal v)
+let serializeSingle   (v: obj) = JsonValue.Float   (System.Convert.ToDouble  v)
+let serializeFloat    (v: obj) = JsonValue.Float   (unbox<float>  v)
+let serializeBoolean  (v: obj) = JsonValue.Boolean (unbox<bool>   v)
+let serializeString   (v: obj) = JsonValue.String  (unbox<string> v)
+let rec serializeArray (t: Type) (v: obj) =
+        let elemType = t.GetElementType ()
+        let o = unbox<System.Collections.IEnumerable> v
+        let en = o.GetEnumerator()
+        let mutable l = []
+        while en.MoveNext() do
+            l <- en.Current :: l
+
+        JsonValue.Array (
+            l
+            |> List.toArray
+            |> Array.map (fun o -> valueSerializer elemType o))
+
+and serializeRecord   (t: Type) (v: obj) =
     let fields = FSharpType.GetRecordFields t
-                 |> Array.map(fun f ->
-                    match f.PropertyType with
-                    | t when t = typeof<int> -> printfn "hello"
-                    | t when t = typeof<single> -> pr)
-    
-    fun (o: obj) ->
-        JsonValue.Record
+    JsonValue.Record (
+        fields
+        |> Array.map (fun pi -> pi.Name, valueSerializer pi.PropertyType (pi.GetValue v)))
 
-let createUnionInstance t = failwith "unimplemented"
-let createTupleInstance t = failwith "unimplemented"
+and valueSerializer (t: Type) (v: obj) =
+    match t with
+    | t when t = typeof<int>        -> serializeInt     v
+    | t when t = typeof<single>     -> serializeSingle  v
+    | t when t = typeof<float>      -> serializeFloat   v
+    | t when t = typeof<bool>       -> serializeBoolean v
+    | t when t = typeof<string>     -> serializeString  v
+    | t when t.IsArray              -> serializeArray t v
+    | t when FSharpType.IsRecord t  -> serializeRecord t v
+    | _ -> failwith "unimplemented"        
 
-let createInstance<'A>() =
-    let t = typeof<'A>
-    if FSharpType.IsRecord t
-    then createRecordInstance t
-    elif FSharpType.IsUnion t
-    then createUnionInstance t
-    elif FSharpType.IsTuple t
-    then createTupleInstance t
-    else failwith "unsupported type"
+let serialize<'T> (v: 'T) = valueSerializer typeof<'T> v
+
 
---
+++ Tests/TestData/Program.fs
@@ -0,0 +1,43 @@
+﻿(*
+** F# Rendering ToolKit
+** Copyright (C) 2015  Wael El Oraiby
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Affero General Public License as
+** published by the Free Software Foundation, either version 3 of the
+** License, or (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Affero General Public License for more details.
+** 
+** You should have received a copy of the GNU Affero General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+open System
+
+open FsRTK.Data
+
+type RecordA = {
+    A : int
+    B : string }
+
+type RecordB = {
+    A : RecordA
+    B : int[] }
+
+[<EntryPoint>]
+let main argv = 
+    let ra = { RecordA.A = 10; B = "Hello World" }
+    let jsRa = Json.serialize ra
+
+    printfn "%O\n------------------" jsRa
+
+    let rb = { RecordB.A = ra; B = [| 10; 7; 3 |] }
+    let jsRb = Json.serialize rb
+
+    printfn "%O\n------------------" jsRb
+
+    0 // return an integer exit code
